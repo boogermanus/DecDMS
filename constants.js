fd1305562acec78f441ec6e457e42240a9ffe9ff
Jordan Woodruff <jordan@neko-chan.local>
2018-01-08 21:59:58 -0600

    new constants

--- constants.js
+++ constants.js
@@ -2,6 +2,15 @@ const minutes = 60;
 const seconds = 3600;
 const minLong = -180.0;
 const maxLong = 180.0;
+const minLat = -90.0;
+const maxLat = 90.0;
 const zero = 0.0;
 
-module.exports = {minutes,seconds,minLong,maxLong};
+module.exports = {
+    minutes,
+    seconds,
+    minLong,
+    maxLong,
+    minLat,
+    maxLat
+};
