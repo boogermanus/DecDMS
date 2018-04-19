6d59dcaba63a09191490c225d30e91f47548533b
Jordan Woodruff <jordan@neko-chan.local>
2018-04-19 18:50:23 -0500

    Update formatting

--- src/DecDMS.js
+++ src/DecDMS.js
@@ -1,7 +1,7 @@
 
 let constant = require('./constants');
 class DecDMS {
-
+//validateUnits------------------------------------------------------------------------------------
     validateUnits(pUnit, pMin, pMax) {
         if(pUnit == null) throw new Error("pUnit cannot be null");
 
@@ -11,19 +11,19 @@ class DecDMS {
 
         return true;
     }
-
+//validateLong-------------------------------------------------------------------------------------
     validateLong(pDegree, pMinutes, pSeconds) {
         this.validateUnits(pDegree, constant.minLong, constant.maxLong);
         this.validateUnits(pMinutes, constant.zero, constant.minutes);
         this.validateUnits(pSeconds, constant.zero, constant.seconds);
     };
-
+//validateLat--------------------------------------------------------------------------------------
     validateLat(pDegree, pMinutes, pSeconds) {
         this.validateUnits(pDegree, constant.minLat, constant.maxLat);
         this.validateUnits(pMinutes, constant.zero, constant.minutes);
         this.validateUnits(pSeconds, constant.zero, constant.secods);
     };
-
+//validateDecimalLong------------------------------------------------------------------------------
     validateDecimalLong(pNumber) {
         let number = Math.floor(pNumber);
         if(number < constant.minLong) throw new Error(`pNumber cannot be less than ${constant.minLong}`);
@@ -32,16 +32,16 @@ class DecDMS {
 
         return true;
     };
-
+//toDecimal----------------------------------------------------------------------------------------
     toDecimal(pDegree, pMinutes, pSeconds) {
         return pDegree + pMinutes/constant.minutes + pSeconds/constant.seconds;
     }
-
+//longToDecimal------------------------------------------------------------------------------------
     longToDecimal(pDegree, pMinutes, pSeconds) {
         this.validateLong(pDegree, pMinutes, pSeconds);
         return this.toDecimal(pDegree, pMinutes, pSeconds);
     };
-
+//lotToDecimal-------------------------------------------------------------------------------------
     latToDecimal(pDegree, pMinutes, pSeconds) {
         this.validateLat(pDegree, pMinutes, pSeconds);
         return this.toDecimal(pDegree, pMinutes, pSeconds);
