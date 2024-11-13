c0a91b132c349df4fe66dfe097a3ff9d00c41643
Jordan Woodruff <boogermanus@gmail.com>
2024-11-13 17:22:03 -0600

    constants

--- DecDMS/services/DecDMSConverter.cs
+++ DecDMS/services/DecDMSConverter.cs
@@ -2,7 +2,13 @@ using DecDMS.Interfaces;
 
 namespace DecDMS.Services;
 
-public class DecDMSConverter : IDecDMSConverter 
+public class DecDMSConverter : IDecDMSConverter
 {
-    
+    public static readonly int MINUTES = 60;
+    public static readonly int SECONDS = 3600;
+    public static readonly float MIN_LONG = -180.0f;
+    public static readonly float MAX_LONG = 180.0f;
+    public static readonly float MIN_LAT = -90.0f;
+    public static readonly float MAX_LAT = 90.0f;
+    public static readonly float ZERO = 0.0f;
 }
