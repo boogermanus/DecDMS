c864524a57002c8ab3989840344ca652ba6f6fae
Jordan Woodruff <boogermanus@gmail.com>
2024-11-13 16:46:40 -0600

    add testing project

---
+++ DecDMS.Testing/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DecDMS.Testing;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
